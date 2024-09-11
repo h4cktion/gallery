4c748747241611d39d634167bf54868ed4c44ac8
hacktion <matthieu.wandolski@gmail.com>
2024-09-11 12:10:14 +0200

    feat(misc): add outputfiletrancingignores

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    outputFileTracingIgnores: ["**/*"], // Ignore tous les fichiers pour la collecte des traces
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
